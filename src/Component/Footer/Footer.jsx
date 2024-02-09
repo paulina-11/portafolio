2d340ea30a844e2471515651048e81e4efaf591b
paulina avila <paulina.avila@ids.com.mx>
2024-02-08 22:58:29 -0600

    actualizacion footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='bg-sky-900 overflow-hidden pb-12 lg:pb-12 pt-10 lg:pt-12 lg:pb-12 flex justify-center items-center text-orange-100 text-xl'>
-      <small>Copyright ©&nbsp;Paulina Avila López 💜</small>
+      <small>Copyright ©&nbsp;Paulina Avila López 💜 2024</small>
     </footer>
   )
 }
